e15df8587cc6cad7c97cb8e84823253b3d801e3a
Gisela Palomar <gslpalomar02@gmail.com>
2024-11-26 10:00:03 +0100

    cambio nombre función y creación docstrings

--- triangulo.py
+++ triangulo.py
@@ -2,47 +2,67 @@ from http.server import HTTPServer , BaseHTTPRequestHandler
 import urllib.parse
 
 
-class LMCDPRequestHandler(BaseHTTPRequestHandler):
+class GSLPVRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        print("------- Contenido del request SELF-------")
-        print(f"path = {self.path}")
-        for key, value in self.__dict__.items():
-            print (f"Atributo de instancia ='{key}' contiene {value}")
-        print("------- Final contenido -------")
+        '''
+        Esta función procesa las solicitudes de tipo GET.
+
+        Funcionalidad: 
+        1. Envía una respuesta HTTP 200 (OK). 
+        2. Añade una cabecera 'Content-Type' con el valor 'text/html'. 
+        3. Finaliza las cabeceras de la respuesta. 
+        4. Escribe el contenido HTML en el cuerpo de la respuesta.
+        '''
         self.send_response(200)
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
         self.wfile.write(bytes(html_calc_triangulo, 'utf-8'))
 
     def do_POST(self):
-        print("------- Contenido del request POST -------")
-        print(f"path = {self.path}")
-        for key, value in self.__dict__.items():
-            print (f"Atributo de instancia ='{key}' contiene {value}")
+        '''
+        Esta función procesa las solicitudes de tipo POST.
+
+        Funcionalidad: 
+        1. Lee la longitud del contenido de la solicitud POST. 
+        2. Extrae los datos enviados en la solicitud y los decodifica. 
+        3. Obtiene la base y la altura del triángulo de los parámetros de la solicitud. 
+        4. Calcula el área del triángulo. 
+        5. Envía la respuesta con el área calculada en formato HTML, usando la función area_triangulo.
+        '''
         content_length = int(self.headers.get('Content-Length'))
         post_data = self.rfile.read(content_length)
-        print(f"post data = {post_data}")
         params = urllib.parse.parse_qs(post_data.decode('utf-8'))
-        print(f"parametros ={params}")
-                # Extract base and height
+        
+            # Extraer la base y la altura
         base = float(params['base'][0])
         altura = float(params['altura'][0])
         print("------- Contenido del request -------")
 
-        # Send the response
+        # Enivar la respuesta
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(bytes(genera_resultado(base, altura), 'utf-8'))
+        self.wfile.write(bytes(area_triangulo(base, altura), 'utf-8'))
 
 
 def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler, puerto=8000):
+    """
+    Descripción de la función:
+    Sirve para configurar y levantar un servidor en un puerto específico.
+
+    Argumetnos:
+    - server_class (type): indica la clase del servidor a usar. 
+    - handler_class (type): indica que modelo de gestores de peticionesa se va a usar.  
+    - puerto (int): Número del puerto en el cual el servidor escucha las solicitudes entrantes. 
+    """
+
     server_address = ('', puerto)
     httpd = server_class(server_address, handler_class)
     print(f"Servidor levantado en http://localhost:{puerto}")
     httpd.serve_forever()
 
-run(handler_class = LMCDPRequestHandler, puerto=8025)
+run(handler_class = GSLPVRequestHandler, puerto=8025)
 
 html_calc_triangulo = """
 <!DOCTYPE html>
@@ -67,8 +87,18 @@ html_calc_triangulo = """
 </html>
 """
 
-def genera_resultado(base, altura):
+def area_triangulo(base, altura):
+    """
+    Descripción de la función usada para calcular el área del triángulo: 
+
+    esta función sirve para devolver el resultado del cálculo matemático.     
+
+    Retorna:
+    - el html con un texto que explica la operación y el resultado de la misma. 
+    """
+
     resultado = base * altura
+    
     html_area= f"""
     <!DOCTYPE html>
     <html lang="es">
